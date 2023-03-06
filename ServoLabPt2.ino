85f4ff5f9457ac66866269fad170a0a66859c771
RileyJon <124637446+RileyJon@users.noreply.github.com>
2023-03-06 18:50:02 -0500

    Update ServoLabPt2.ino

--- ServoLabPt2.ino
+++ ServoLabPt2.ino
@@ -12,7 +12,7 @@ void setup() {
 
 void loop() {
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  val = map(val, 0, 1023, 180, 0);     // These can be flipped to change the direction of the motor movement
   myservo.write(val);                  // sets the servo position according to the scaled value
   delay(15);                           // waits for the servo to get there
 }
